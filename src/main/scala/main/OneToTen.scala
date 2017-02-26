d293a93211eb2fb73bcaba64870ac82d1a5481fe
Anthony Garo <agaro1121@gmail.com>
2017-02-25 20:05:39 -0500

    refactoring = 9 and 10

--- src/main/scala/main/OneToTen.scala
+++ src/main/scala/main/OneToTen.scala
@@ -1,24 +1,28 @@
 package main
 
+import scala.annotation.tailrec
 import scala.collection.immutable.{Queue, Stack}
 
-object OneToTen {
-
-  def one[A](list: List[A]): A = {
+object one {
+  def last[A](list: List[A]): A = {
     require(list != Nil, "Your List is empty !!!")
     (list: @unchecked) match {
       case x :: Nil => x
-      case _ :: xs => one(xs)
+      case _ :: xs => last(xs)
     }
   }
+}
 
-  def two[A](list: List[A]): A = list match {
+object two {
+  def penultimate[A](list: List[A]): A = list match {
     case x :: _ :: Nil => x
-    case _ :: xs => two(xs)
+    case _ :: xs => penultimate(xs)
     case _ => throw new NoSuchElementException
   }
+}
 
-  def three[T](index: Int, list: List[T]): T = {
+object three {
+  def nth[T](index: Int, list: List[T]): T = {
     require(index >= 0)
 
     def loop(count: Int, list: List[T]): T = {
@@ -32,9 +36,11 @@ object OneToTen {
 
     loop(0, list)
   }
+}
 
-  def four2[T](list: List[T]): Int = list.foldLeft(0)((acc, _) ⇒ acc + 1)
-  def four[T](list: List[T]): Int = {
+object four {
+  def lengthWithFoldLeft[T](list: List[T]): Int = list.foldLeft(0)((acc, _) ⇒ acc + 1)
+  def length[T](list: List[T]): Int = {
     def loop(acc: Int, list: List[T]): Int = {
       list match {
         case Nil ⇒ 0
@@ -45,52 +51,91 @@ object OneToTen {
     loop(0, list)
   }
 
-  def five[T](list: List[T]): List[T] = {
+}
+
+object five {
+  def reverse[T](list: List[T]): List[T] = {
     list.foldLeft(Nil: List[T])((acc, elem) ⇒ elem :: acc)
   }
+}
 
-  def six[T](list: List[T]): Boolean = {
+object six {
+  def isPalindrome[T](list: List[T]): Boolean = {
     list.reverse == list
   }
+}
 
-  def seven[T](list: List[T]): List[T] = {
+object seven {
+  def flatten[T](list: List[T]): List[T] = {
     list.foldLeft(Nil: List[T]) {
       (acc, elem) ⇒
         elem match {
-          case l: List[_] ⇒ (acc ++ seven(l)).asInstanceOf[List[T]]
+          case l: List[_] ⇒ (acc ++ flatten(l)).asInstanceOf[List[T]]
           case e ⇒ acc :+ e
         }
     }
   }
+}
 
-  def eight[T](list: List[T]): List[T] = {
+object eight {
+
+  def compress[T](list: List[T]): List[T] = {
     @scala.annotation.tailrec
     def loop(list: List[T], acc: List[T]): List[T] = {
       list match {
         case Nil ⇒ acc
-        case x::xs ⇒ loop(xs,
-          if(!acc.headOption.contains(x)) x :: acc
+        case x :: xs ⇒ loop(
+          xs,
+          if (!acc.headOption.contains(x)) x :: acc
           else acc
         )
       }
     }
-
     loop(list, Nil).reverse
   }
 
-  def eight2[T](list: List[T]): List[T] = {
-    @scala.annotation.tailrec
-    def loop(list: List[T], acc: Stack[T]): Stack[T] = {
+}
+
+object nine {
+
+  def pack[T](list: List[T]): List[List[T]] = {
+    @tailrec
+    def loop(list: List[T], acc: List[List[T]]): List[List[T]] = {
       list match {
-        case Nil ⇒ acc
-        case x::xs ⇒ loop(xs,
-          if(!acc.headOption.contains(x)) acc.push(x)
-          else acc
-        )
+        case Nil ⇒ acc.reverse
+        case x :: xs ⇒
+          val (dupes, restAfterTakingDupes) = xs.span(_ == x)
+          loop(restAfterTakingDupes, (x :: dupes) :: acc)
       }
     }
+    loop(list, Nil)
+  }
 
-    loop(list, Stack.empty).toList.reverse
+}
+
+object ten {
+
+  def encodeWithFoldLeft[T](list: List[T]): List[(Int, T)] = {
+    nine.pack(list).foldLeft(Nil: List[(Int, T)])((acc, elem) ⇒ (elem.size, elem.head) :: acc).reverse
+  }
+
+  def encodeWithFoldRight[T](list: List[T]): List[(Int, T)] = {
+    nine.pack(list).foldRight(Nil: List[(Int, T)])((elem, acc) ⇒ (elem.size, elem.head) :: acc)
+  }
+
+  def encode[T](list: List[T]): List[(Int, T)] = {
+    @tailrec
+    def loop(list: List[List[T]], acc: List[(Int, T)]): List[(Int, T)] = {
+      list match {
+        case Nil ⇒ acc.reverse
+        case x :: xs ⇒ loop(xs, (x.size, x.head) :: acc)
+      }
+    }
+    loop(nine.pack(list), Nil)
+  }
+
+  def encodeWithMap[T](list: List[T]): List[(Int, T)] = {
+    nine.pack(list).map(l ⇒ (l.size, l.head))
   }
 
 }
--- src/test/scala/Tests.scala
+++ src/test/scala/Tests.scala
@@ -1,55 +1,63 @@
+import main._
 import org.scalatest.{Matchers, Pending, WordSpec}
 
 class Tests extends WordSpec with Matchers {
 
   "First 10 Problems should be successfully implemented" should {
-    import main.OneToTen._
     val testList = List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
 
     "1. Find the last element of the list" in {
-      one(testList) shouldBe 10
+      one.last(testList) shouldBe 10
     }
 
     "2. Find the last but one element of a list" in {
-      two(testList) shouldBe 9
+      two.penultimate(testList) shouldBe 9
     }
 
     "3. Find the Kth element of a list" in {
-      three(3, testList) shouldBe 4
+      three.nth(3, testList) shouldBe 4
     }
 
     "4. Find the number of elements of a list" in {
-      four(testList) shouldBe 10
-      four2(testList) shouldBe 10
+      four.length(testList) shouldBe 10
+      four.lengthWithFoldLeft(testList) shouldBe 10
     }
 
     "5. Reverse a list" in {
-      five(testList) shouldBe List(10, 9, 8, 7, 6, 5, 4, 3, 2, 1)
+      five.reverse(testList) shouldBe List(10, 9, 8, 7, 6, 5, 4, 3, 2, 1)
       testList.reverse shouldBe List(10, 9, 8, 7, 6, 5, 4, 3, 2, 1)
     }
 
     "6. Find out whether a list is a palindrome" in {
-      six(testList) shouldBe false
-      six(List(1, 2, 2, 1)) shouldBe true
+      six.isPalindrome(testList) shouldBe false
+      six.isPalindrome(List(1, 2, 2, 1)) shouldBe true
     }
 
     "7. Flatten a nested list structure" in {
-      seven(List(List(1, 1), 2, List(3, List(5, 8)))) shouldBe List(1, 1, 2, 3, 5, 8)
+      seven.flatten(List(List(1, 1), 2, List(3, List(5, 8)))) shouldBe List(1, 1, 2, 3, 5, 8)
     }
 
     "8. Eliminate consecutive duplicates of list elements" in {
       val input = List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)
       val output = List('a, 'b, 'c, 'a, 'd, 'e)
-      eight(input) shouldBe output
-      eight2(input) shouldBe output
+      eight.compress(input) shouldBe output
     }
 
     "9. Pack consecutive duplicates of list elements into sublists" in {
-      Pending
+      val input = List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)
+      val expectedOutput = List(List('a, 'a, 'a, 'a), List('b), List('c, 'c), List('a, 'a), List('d), List('e, 'e, 'e, 'e))
+
+      nine.pack(input) shouldBe expectedOutput
     }
 
     "10. Run-length encoding of a lis" in {
-      Pending
+      val input = List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)
+      val expectedOutput: List[(Int, Symbol)] = List((4, 'a), (1, 'b), (2, 'c), (2, 'a), (1, 'd), (4, 'e))
+
+      ten.encodeWithFoldLeft(input) shouldBe expectedOutput
+      ten.encodeWithFoldRight(input) shouldBe expectedOutput
+      ten.encode(input) shouldBe expectedOutput
+      ten.encodeWithMap(input) shouldBe expectedOutput
     }
 
   }
